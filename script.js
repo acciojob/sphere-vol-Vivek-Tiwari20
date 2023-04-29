e632405dc9f443defe6cc1202bdcfe7d1dd6fce3
Vivek Tiwari <102945885+Vivek-Tiwari20@users.noreply.github.com>
2023-04-29 16:39:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const radiusInput = document.getElementById('radius');
 const volumeOutput = document.getElementById('volume');
 const radius = parseFloat(radiusInput.value);
 const volume = (4/3)*Math.PI*Math.pow(radius, 3);
-volumeOutput.value = volume;
+volumeOutput.value = parseFloat(volume.toFixed(4));
 }
 
 document.getElementById('MyForm').addEventListener('submit', function(event) {
