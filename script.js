edb756c596ebe38d73fda1a1272b60e7a95af0f7
Vivek Tiwari <102945885+Vivek-Tiwari20@users.noreply.github.com>
2023-04-29 16:22:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function volume_sphere() {
 const radiusInput = document.getElementById('radius');
 const volumeOutput = document.getElementById('volume');
 const radius = parseFloat(radiusInput.value);
-const volume = (4/3)*Math.PI Math.pow(radius, 3);
+const volume = (4/3)*Math.PI*Math.pow(radius, 3);
 volumeOutput.value = volume.toFixed(2);
 } 
 
